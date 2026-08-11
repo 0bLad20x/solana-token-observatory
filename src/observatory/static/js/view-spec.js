7ae1b7994145ce3c11eb3d3694644ce6eb39b304
0bLad20x <0bLad20x@proton.me>
2026-08-11 16:37:50 +0200

    align frontend with static V3 reset

--- src/observatory/static/js/view-spec.js
+++ src/observatory/static/js/view-spec.js
@@ -1,14 +1,19 @@
-export const DEFAULT_VIEW_ID = "launchpad-cluster";
+export const DEFAULT_VIEW_ID = "launchpad-overview-focus";
 
 export const VIEW_PRESETS = Object.freeze({
   [DEFAULT_VIEW_ID]: Object.freeze({
     id: DEFAULT_VIEW_ID,
     type: "bubble",
-    layout: "cluster",
-    x: null,
-    y: null,
-    size: "market_cap",
-    color: "launchpad",
-    group: "launchpad",
+    layout: "overview-focus",
+    overview: Object.freeze({
+      group: "launchpad",
+      size: "token_count",
+      color: "launchpad",
+    }),
+    focus: Object.freeze({
+      rank: "market_cap",
+      size: "market_cap",
+      color: "launchpad",
+    }),
   }),
 });
